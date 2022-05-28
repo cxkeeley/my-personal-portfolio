2b31e5a6e1f848eea1a7402c5a8d2b0ef4fad520
cxkeeley <cxkeeley@gmail.com>
2022-05-28 18:05:08 +0700

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,42 +6,42 @@ import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
-    const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-    return (
-        <nav className='app__navbar'>
-            <div className='app__navbar-logo'>
-                <img src={images.logo} alt='logo' />
-            </div>
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='logo' />
+      </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map(item => (
+          <li className='app__flex' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <div>
+            <HiX onClick={() => setToggle(false)} />
             <ul className='app__navbar-links'>
-                {['home', 'about', 'work', 'skills', 'contact'].map(item => (
-                    <li className='app__flex' key={`link-${item}`}>
-                        <div />
-                        <a href={`#${item}`}>{item}</a>
-                    </li>
-                ))}
+              {['home', 'about', 'work', 'skills', 'contact'].map(item => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
             </ul>
+          </div>
+        )}
+      </div>
 
-            <div className='app__navbar-menu'>
-                <HiMenuAlt4 onClick={() => setToggle(true)} />
-
-                {toggle && (
-                    <div>
-                        <HiX onClick={() => setToggle(false)} />
-                        <ul className='app__navbar-links'>
-                            {['home', 'about', 'work', 'skills', 'contact'].map(item => (
-                                <li key={item}>
-                                    <a href={`#${item}`} onClick={() => setToggle(false)}>
-                                        {item}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-            </div>
-
-            {/* <div className='app__navbar-menu'>
+      {/* <div className='app__navbar-menu'>
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 
         {toggle && (
@@ -62,8 +62,8 @@ const Navbar = () => {
           </motion.div>
         )}
       </div> */}
-        </nav>
-    );
+    </nav>
+  );
 };
 
 export default Navbar;
