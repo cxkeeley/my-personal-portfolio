7f2c054570b9425268506f9033b8470241fffd34
cxkeeley <cxkeeley@gmail.com>
2022-06-01 21:18:07 +0700

    commit

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
-import { BsTwitter, BsInstagram, BsLinkedin, BsGithub, BsDribbble } from 'react-icons/bs';
+import {
+  BsTwitter,
+  BsInstagram,
+  BsLinkedin,
+  BsGithub,
+  BsDribbble
+} from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
-    return (
-        <div className='app__social'>
-            <div>
-                <BsTwitter />
-            </div>
-            <div>
-                <FaFacebookF />
-            </div>
-            <div>
-                <BsInstagram />
-            </div>
-            <div>
-                <a href='https://www.linkedin.com/in/darwin-22467a232/'>
-                    <BsLinkedin />
-                </a>
-            </div>
-            <div>
-                <a href='https://github.com/cxkeeley'>
-                    <BsGithub />
-                </a>
-            </div>
-            <div>
-                <BsDribbble />
-            </div>
-        </div>
-    );
+  return (
+    <div className='app__social'>
+      <div>
+        <a href='https://twitter.com/cxkeeley'>
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <FaFacebookF />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+      <div>
+        <a href='https://www.linkedin.com/in/darwin-keeley/'>
+          <BsLinkedin />
+        </a>
+      </div>
+      <div>
+        <a href='https://github.com/cxkeeley'>
+          <BsGithub />
+        </a>
+      </div>
+      <div>
+        <BsDribbble />
+      </div>
+    </div>
+  );
 };
 
 export default SocialMedia;
