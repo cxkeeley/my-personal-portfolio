db98e826e46e226f04e86f9d5e0b0cdbe9f8ac0c
cxkeeley <cxkeeley@gmail.com>
2022-07-20 21:33:02 +0700

    little change

--- src/components/Navbar/Navbar-rev.jsx
+++ src/components/Navbar/Navbar-rev.jsx
@@ -40,28 +40,6 @@ const Navbar = () => {
           </div>
         )}
       </div>
-
-      {/* <div className='app__navbar-menu'>
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
-
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul className='app__navbar-links'>
-              {['home', 'about', 'work', 'skills', 'contact'].map(item => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
-        )}
-      </div> */}
     </nav>
   );
 };
